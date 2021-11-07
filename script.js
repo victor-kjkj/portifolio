388cfa05a8059077aef76ddac42840a7822c4a35
victor-kjkj <victor.1102.vh@gmail.com>
2021-11-07 18:54:00 -0300

    js

---
+++ script.js
@@ -0,0 +1,4 @@
+var hamburguer=document.querySelector(".hamburger");
+
+hamburguer.addEventListener("click", function(){document.querySelector("sidebar-ou-barra-lateral").classList.toggle("show-menu")
+})
